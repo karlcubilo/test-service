8ae099b75cc01746a30a366b046fdf2f490190c8
Karl Cubilo <karl.cubilo@gmail.com>
2024-03-06 02:04:55 +0800

    adding feature test

--- src/main/java/com/te/testservice/admin/service/AdminService.java
+++ src/main/java/com/te/testservice/admin/service/AdminService.java
@@ -2,18 +2,17 @@ package com.te.testservice.admin.service;
 
 import com.te.testservice.admin.aspect.CreateAdminValidation;
 import com.te.testservice.admin.builder.AdminBuilder;
-import com.te.testservice.admin.exception.AdminException;
-import com.te.testservice.admin.exception.AdminExceptionMessage;
+import com.te.testservice.admin.model.Admin;
 import com.te.testservice.repository.dao.AdminRepository;
 import com.te.testservice.repository.entity.AdminEntity;
-import com.te.testservice.admin.model.Admin;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -36,6 +35,7 @@ public class AdminService {
     }
 
     @CreateAdminValidation
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public Admin createAdmin(Admin admin) {
         AdminEntity adminEntity =adminRepository.save(adminBuilder.build(admin));
         admin.setId(adminEntity.getId());
